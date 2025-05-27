a155a228f2b36c355e3c63975c43e76706e3c3a1
biestro <albertorbiestro@gmail.com>
2025-05-27 14:37:44 -0500

    copied from other project

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # BetoThemes
 I like mathematica, sue me.
+
+# Installation
+```julia
+using Pkg
+Pkg.add(url="https://github.com/biestro/BetoThemes.git")
+Pkg.update("BetoThemes")
+using MyMakieThemes
+```
+
+# Dependencies
+```julia
+Makie # CairoMakie backend
+Colors
+LaTeXStrings
+MathTeXEngine
+ColorSchemes
+```
--- tests/plottest.jl
+++ tests/plottest.jl
@@ -34,12 +34,11 @@ H = circshift(diagm(ones(N)) * L , 3) + circshift(diagm(ones(N)) * R , -2)
 evals_orig = eigvals(H)
 
 # plotting
-let
+
 fig = Figure(theme=BetoThemes.default(:math; sw=0.0), fontsize=20)
 
 cmap = :phase
 gl = GridLayout(fig[1,1])
-gr = GridLayout(fig[1,2])
 ax = Axis(gl[1,1], xlabel=fmt("Re \$E_n\$"), ylabel=fmt("Im \$E_n\$"))
 
 # grids
@@ -48,11 +47,10 @@ ax = Axis(gl[1,1], xlabel=fmt("Re \$E_n\$"), ylabel=fmt("Im \$E_n\$"))
 sc=scatter!(ax, real(evals), imag(evals), color=colors, colormap=cmap)
 scatter!(ax, real(evals_orig), imag(evals_orig), color=Makie.to_colormap(cmap)[1], 
                        strokewidth=1.0, label = L"\phi=0")
-Colorbar(gr[1,1], sc, label = L"\phi")
+Colorbar(gl[1,2], sc, label = L"\phi")
 
-
+colgap!(gl, 1, 0)
 axislegend(ax)
-ax.aspect=DataAspect()
+ax.aspect=1
 resize_to_layout!(fig)
 fig
-end
